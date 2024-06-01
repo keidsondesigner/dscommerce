3fd43d478532799b0e8bae145fdce33560f3887b
Keidson Roby <keidsondesigner@gmail.com>
2024-06-01 15:45:53 -0400

    refactor: :recycle: Garantindo que o campo email seja único no banco de dados

--- src/main/java/com/keidsonroby/dscommerce/entities/User.java
+++ src/main/java/com/keidsonroby/dscommerce/entities/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +20,9 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(unique = true) // campo email vai ser único
   private String email;
+
   private String password;
   private String name;
   private String phone;
