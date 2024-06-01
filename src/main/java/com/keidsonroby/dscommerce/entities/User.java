beb5bf33abe7bdc188296349a10f320c1f3d9372
Keidson Roby <keidsondesigner@gmail.com>
2024-06-01 00:00:05 -0400

    feat: :sparkles: Cria entidade User, para mapeamento e persistência de dados de usuário;

---
+++ src/main/java/com/keidsonroby/dscommerce/entities/User.java
@@ -0,0 +1,83 @@
+package com.keidsonroby.dscommerce.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String email;
+  private String password;
+  private String name;
+  private String phone;
+  private LocalDate birthDate;
+
+  public User() { }
+
+  public User(Long id, String email, String password, String name, String phone, LocalDate birthDate) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.phone = phone;
+    this.birthDate = birthDate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+}
